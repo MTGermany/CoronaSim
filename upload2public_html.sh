16a12e181f982802f1b2fdf71f131ff07ad9e0be
treibi <treiber@vwi.tu-dresden.de>
2020-04-15 11:59:28 +0200

    with info

--- upload2public_html.sh
+++ upload2public_html.sh
@@ -3,6 +3,7 @@
 target=$HOME/public_html/CoronaSim
 
 cp -r *.css *.js *.html figs $target
+cp data/coronaWorldwide.json $target/data
 chmod 604 `find $target -type f`
 chmod 701 `find $target -type d` 
 
