e9f430fb547891f02368fde1b22f4dd51dbabae4
treibi <treiber@vwi.tu-dresden.de>
2020-05-08 12:05:17 +0200

    async await useful?

--- demo_await.js
+++ demo_await.js
@@ -1,14 +1,13 @@
-async function http(request){
+
+async function http(
+  request: RequestInfo
+): Promise<any> {
   const response = await fetch(request);
   const body = await response.json();
   return body;
 }
 
 // example consuming code
-
-function demo(){
-  const data = await http(
-    "https://pomber.github.io/covid19/timeseries.json"
-  );
-  console.log("data=",data);
-}
+const data = await http(
+  "https://jsonplaceholder.typicode.com/todos"
+);
